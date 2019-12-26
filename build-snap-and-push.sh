261de05e95346c3912bfe7099abb638073330a12
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2019-12-26 16:45:47 +0300

    #15 Update snap build script

--- build-snap-and-push.sh
+++ build-snap-and-push.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 snapcraft
-snapcraft push --release=stable dynocsv_1.0.0_amd64.snap
+snapcraft push --release=stable dynocsv_1.1.0_amd64.snap
 
-# sudo snap install --dangerous dynocsv_1.0.0_amd64.snap
+# sudo snap install --dangerous dynocsv_1.1.0_amd64.snap
 
 # Debug
 # snap debug confinement
