d81b1d001de172c8e7a5abf8ae656fba44b18ea5
Aliaksandr Kazlou <aliaksandr.kazlou@gmail.com>
2019-12-29 14:39:11 +0300

    #10 Update to the next release version

--- build-snap-and-push.sh
+++ build-snap-and-push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 snapcraft
-snapcraft push --release=stable dynocsv_1.1.1_amd64.snap
+snapcraft push --release=stable dynocsv_1.1.2_amd64.snap
 
 # sudo snap install --dangerous dynocsv_1.1.0_amd64.snap
 
